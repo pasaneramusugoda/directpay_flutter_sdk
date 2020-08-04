f47a8688228f8becedf97d2085d8bc5090298bbd
Deeptha Senanayake <chalithadeeptha@gmail.com>
2020-08-04 13:57:24 +0530

    tested in test environment

--- android/src/main/java/com/paymedia/flutter_mpgs_sdk/MPGS/ScannerActivity.java
+++ android/src/main/java/com/paymedia/flutter_mpgs_sdk/MPGS/ScannerActivity.java
@@ -26,10 +26,10 @@ public class ScannerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_scanner);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setTitle("Scan to Add Card");
-        setContentView(R.layout.activity_scanner);
 
         mRecognizer = new BlinkCardRecognizer();
         // bundle recognizers into RecognizerBundle
